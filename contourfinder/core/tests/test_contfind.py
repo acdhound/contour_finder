44a5a803f15ba80d7e5470f59be3b8e646c9b976
acdhound <adivanov95@mail.ru>
2016-10-20 21:59:44 +0300

    improve test

--- contourfinder/core/tests/test_contfind.py
+++ contourfinder/core/tests/test_contfind.py
@@ -5,19 +5,34 @@ from ..contfind import MorphContourFinder
 
 class TestMorphContourFinder(TestCase):
 
-    def __init__(self, methodName='runTest'):
-        super(TestMorphContourFinder, self).__init__(methodName)
-        self.contFinder = MorphContourFinder.withThresholdAndKernelInside(127, np.ones((3, 3), np.uint8))
-
-    def test_square(self):
+    def createSquare(self):
         img = np.zeros([100, 100, 1], np.uint8)
         img[50:80, 50:80] = np.ones([30, 30, 1]) * 200
+        return img
 
+    def assertPointValue(self, img, x, y, value):
+        self.assertEqual(img[y, x], value, "unexpected value of pixel ({0},{1})".format(x, y))
+
+    def assertSquareContour(self, img, x0, y0, d, value):
+        for y in range(y0, y0 + d):
+            self.assertPointValue(img, x0, y, 255)
+            self.assertPointValue(img, x0 + d, y, 255)
+        for x in range(x0, x0 + d):
+            self.assertPointValue(img, x, y0, 255)
+            self.assertPointValue(img, x, y0 + d, 255)
+
+    def test_square_inside(self):
+        self.contFinder = MorphContourFinder.withThresholdAndKernelInside(127, np.ones((3, 3), np.uint8))
+
+        img = self.createSquare()
         contImg = self.contFinder.getContour(img)
 
-        self.assertEqual(contImg[0, 0], 0)
-        self.assertEqual(contImg[50, 50], 255)
-        self.assertEqual(contImg[79, 79], 255)
-        self.assertEqual(contImg[50, 79], 255)
-        self.assertEqual(contImg[79, 50], 255)
-        self.assertEqual(contImg[65, 65], 0)
+        self.assertSquareContour(contImg, 50, 50, 29, 255)
+
+    def test_square_outside(self):
+        self.contFinder = MorphContourFinder.withThresholdAndKernelOutside(127, np.ones((3, 3), np.uint8))
+
+        img = self.createSquare()
+        contImg = self.contFinder.getContour(img)
+
+        self.assertSquareContour(contImg, 49, 49, 31, 255)

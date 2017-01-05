7f8e4c486e59b2c68ab56d9a4a2ef3e4197275b6
acdhound <adivanov95@mail.ru>
2017-01-05 22:56:49 +0300

    reduce test image, save inner and outer edges

--- contourfinder/main.py
+++ contourfinder/main.py
@@ -18,7 +18,9 @@ if str(sys.argv[2]) == 'canny':
 elif str(sys.argv[2]) == "morph":
     threshold = float(sys.argv[3])
     kSize = int(sys.argv[4])
-    cv2.imwrite('edges_morph.bmp',
+    cv2.imwrite('edges_morph_outside.bmp',
                 ClosingMorphEdgeDetector.withThresholdAndKSize(threshold, kSize, False).getEdges(img))
+    cv2.imwrite('edges_morph_inside.bmp',
+                ClosingMorphEdgeDetector.withThresholdAndKSize(threshold, kSize, True).getEdges(img))
 else:
     raise Exception('unknown method: ' + str(sys.argv[2]))

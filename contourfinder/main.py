cb543d3e16f778ad41607e6295b6a95098b1d027
acdhound <adivanov95@mail.ru>
2017-01-11 23:00:46 +0300

    refactor main

--- contourfinder/main.py
+++ contourfinder/main.py
@@ -4,20 +4,19 @@ import os
 from contourfinder.core.detect import EdgeDetectorFactory
 from contourfinder.compare.compare import EdgeComparatorFactory
 
+edgeComparator = EdgeComparatorFactory().create()
+
 
 def read_gray_img(path):
     return cv2.cvtColor(cv2.imread(path, 1), cv2.COLOR_BGR2GRAY)
 
 
-if __name__ != '__main__' or len(sys.argv) < 4:
-    print 'invalid arguments'
-    exit(1)
+def save_quality_index(actual_edges, expected_edges, name='result'):
+    result = edgeComparator.compare(actual_edges, expected_edges)[0]
+    f = open(name + '.txt', 'w')
+    f.write(str(result))
+    f.close()
 
-if str(sys.argv[1]) == 'quality':
-    actual = read_gray_img(str(sys.argv[2]))
-    expected = read_gray_img(str(sys.argv[3]))
-    print 'Result: {0}'.format(EdgeComparatorFactory().create().compare(actual, expected)[0])
-    exit(0)
 
 imgPath = str(sys.argv[1])
 img = read_gray_img(imgPath)
@@ -27,14 +26,28 @@ edgeDetectorFactory = EdgeDetectorFactory()
 if str(sys.argv[2]) == 'canny':
     threshold1 = int(sys.argv[3])
     threshold2 = int(sys.argv[4])
-    cv2.imwrite(imgName + '__edges_canny.bmp',
-                edgeDetectorFactory.createCannyEdgeDetector(threshold1, threshold2).getEdges(img))
+
+    canny_edges = edgeDetectorFactory.createCannyEdgeDetector(threshold1, threshold2).getEdges(img)
+    cv2.imwrite(imgName + '__edges_canny.bmp', canny_edges)
+
+    expected_edge_area = read_gray_img(str(sys.argv[5]))
+    save_quality_index(canny_edges, expected_edge_area, 'result_canny')
+
+    exit(0)
 elif str(sys.argv[2]) == "morph":
     threshold = float(sys.argv[3])
     kSize = int(sys.argv[4])
-    cv2.imwrite(imgName + '__edges_morph_outside.bmp',
-                edgeDetectorFactory.createClosingMorphEdgeDetector(threshold, kSize, False).getEdges(img))
-    cv2.imwrite(imgName + '__edges_morph_inside.bmp',
-                edgeDetectorFactory.createClosingMorphEdgeDetector(threshold, kSize, True).getEdges(img))
+
+    morph_edges_outside = edgeDetectorFactory.createClosingMorphEdgeDetector(threshold, kSize, False).getEdges(img)
+    cv2.imwrite(imgName + '__edges_morph_outside.bmp', morph_edges_outside)
+
+    morph_edges_inside = edgeDetectorFactory.createClosingMorphEdgeDetector(threshold, kSize, True).getEdges(img)
+    cv2.imwrite(imgName + '__edges_morph_inside.bmp', morph_edges_inside)
+
+    expected_edge_area = read_gray_img(str(sys.argv[5]))
+    save_quality_index(morph_edges_outside, expected_edge_area, 'result_morph_outside')
+    save_quality_index(morph_edges_inside, expected_edge_area, 'result_morph_inside')
+
+    exit(0)
 else:
     raise Exception('unknown method: ' + str(sys.argv[2]))

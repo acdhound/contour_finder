470f6aa4af9a110b691a543ebb84f19279a2ee95
acdhound <adivanov95@mail.ru>
2017-02-25 00:47:59 +0300

    refactor fragments matching module, improve gap line calculation, add test samples of fragmented documents

--- document_restorer/match_pieces.py
+++ document_restorer/match_pieces.py
@@ -1,16 +1,44 @@
 import cv2
+import numpy as np
 
 from document_restorer.edges.detect import EdgeDetectorFactory
-from document_restorer.restore.match import BinaryEdgeMatcher
-
-img1 = cv2.cvtColor(cv2.imread('../resources/img/piece3.png', 1), cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(cv2.imread('../resources/img/piece4.png', 1), cv2.COLOR_BGR2GRAY)
+from document_restorer.restore.match import VerticalShiftFragmentsConnector
+from document_restorer.restore.match import HarrisFragmentsContentMatcher
 
 detector = EdgeDetectorFactory().createClosingMorphEdgeDetector(100, 10)
-matcher = BinaryEdgeMatcher(detector)
-result = matcher.matchEdges(img1, img2)
+connector = VerticalShiftFragmentsConnector(detector)
+content_matcher = HarrisFragmentsContentMatcher()
 
-print result[0]
-cv2.imshow('stuck pieces', result[1])
-cv2.imshow('gap line', result[2])
-cv2.waitKey(0)
+
+def match_pieces(piece1, piece2):
+    connection = connector.connectFragments(piece1, piece2)
+    content_match_result_1 = content_matcher.matchFragmentsContent(connection.stuck_fragments, connection.gap_line)
+    return content_match_result_1, connection.adjacency
+
+
+def save_normalized(mat, name):
+    img = np.zeros(mat.shape, np.uint8)
+    img = cv2.normalize(mat, img, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    cv2.imwrite(name, img)
+
+pieces_num = 33
+images = []
+for n in range(1, pieces_num + 1):
+    images.append(cv2.cvtColor(cv2.imread('../resources/img/documents/2/piece{0}.png'.format(n), 1), cv2.COLOR_BGR2GRAY))
+
+results_content = np.zeros([len(images), len(images), 1])
+results_edges = np.zeros([len(images), len(images), 1])
+for i in range(0, len(images)):
+    for j in range(0, len(images)):
+        # if j == i + 1 or ((i, j) in [(3, 2), (4, 0), (5, 1), (2, 4), (3, 0), (0, 5), (4, 1)]):
+        #     content_matcher.write_result_to = 'match_{0}_{1}.png'.format(i + 1, j + 1)
+        #     need_print_result = True
+        # else:
+        #     content_matcher.write_result_to = None
+        result = match_pieces(images[i], images[j])
+        results_content[i, j] = result[0]
+        results_edges[i, j] = result[1]
+
+save_normalized(results_content, 'content_results.bmp')
+save_normalized(results_edges, 'edges_results.bmp')
+exit(0)
--- document_restorer/restore/match.py
+++ document_restorer/restore/match.py
@@ -3,76 +3,100 @@ import cv2
 from ..operations.util import count_nonzero, copy_to
 
 
-class EdgeMatcher(object):
+class FragmentsConnector(object):
     """
-        Matches edges of two images representing pieces of a paper with text broken into horizontal strips.
-    Edges of pieces are shifted relative to each other vertically to find position where they most look like
-    two adjacent pieces of rotten paper put together.
-        The result is a tuple of three elements. First is the value of numeric coefficient representing edges'
-    adjacency, second is the image representing two pieces stuck together and third is corresponding gap line
-    between pieces.
+        Tries to connect two fragments of the paper text document assuming the document was broken
+    into horizontal strips. Analyses fragments' edges and calculates numerical coefficient representing
+    fragments' adjacency.
     """
 
-    def matchEdges(self, top_piece, bottom_piece):
+    def connectFragments(self, top_fragment, bottom_fragment):
         pass
 
 
-def get_bottom_half(img):
-    return img[img.shape[0] / 2:, :]
+class FragmentsConnection(object):
+
+    def __init__(self, adjacency, stuck_fragments, gap_line):
+        self.adjacency = adjacency
+        self.stuck_fragments = stuck_fragments
+        self.gap_line = gap_line
 
 
-def get_top_half(img):
-    return img[:img.shape[0] / 2, :]
-
-
-class BinaryEdgeMatcher(EdgeMatcher):
-    """
-        Extracts binary edges from pieces and calculates adjacency coefficient by applying logical AND and counting
-    number of non-zero pixels.
-    """
+class VerticalShiftFragmentsConnector(FragmentsConnector):
 
     def __init__(self, edge_detector):
         self.edge_detector = edge_detector
 
-    def matchEdges(self, top_piece, bottom_piece):
-        if top_piece.shape[1] != bottom_piece.shape[1]:
+    def connectFragments(self, top_fragment, bottom_fragment):
+        if top_fragment.shape[1] != bottom_fragment.shape[1]:
             raise Exception("Pieces of the same width expected")
 
-        top_edge = self.edge_detector.getEdges(top_piece)
-        bottom_edge = self.edge_detector.getEdges(bottom_piece)
+        top_edge = self.edge_detector.getEdges(top_fragment)
+        bottom_edge = self.edge_detector.getEdges(bottom_fragment)
 
-        top_edge_bottom = get_bottom_half(top_edge)
-        bottom_edge_top = get_top_half(bottom_edge)
+        top_edge_bottom = self.__get_bottom_half(top_edge)
+        bottom_edge_top = self.__get_top_half(bottom_edge)
         im1 = np.zeros([top_edge_bottom.shape[0] + bottom_edge_top.shape[0], top_edge.shape[1]], np.uint8)
         copy_to(top_edge_bottom, im1, 0, 0)
 
-        max_nonzero, delta_for_max, im_product_for_max = 0, 0, None
+        max_adjacency, delta_for_max, gap_lines = 0, 0, None
 
         for delta in range(-bottom_edge_top.shape[0] + 1, top_edge_bottom.shape[0]):
             im2 = np.zeros([im1.shape[0], im1.shape[1]], np.uint8)
             copy_to(bottom_edge_top, im2, 0, delta)
-
-            im_product = cv2.bitwise_and(im1, im2)
-            nonzero = count_nonzero(im_product)
-            if nonzero > max_nonzero:
-                max_nonzero = nonzero
+            common_edge = cv2.bitwise_and(im1, im2)
+            adjacency = float(count_nonzero(common_edge)) / float(common_edge.shape[1])
+            if adjacency > max_adjacency:
+                max_adjacency = adjacency
                 delta_for_max = delta
-                im_product_for_max = im_product
-            elif im_product_for_max is None:
-                im_product_for_max = im_product
+                gap_lines = [im1, im2]
+            elif gap_lines is None:
+                gap_lines = [im1, im2]
 
-        top_piece_bottom = get_bottom_half(top_piece)
-        bottom_piece_top = get_top_half(bottom_piece)
+        top_fragment_bottom = self.__get_bottom_half(top_fragment)
+        bottom_fragment_top = self.__get_top_half(bottom_fragment)
         im1 = np.zeros([im1.shape[0], im1.shape[1]], np.uint8)
         im2 = np.copy(im1)
-        copy_to(top_piece_bottom, im1, 0, 0)
-        copy_to(bottom_piece_top, im2, 0, delta_for_max)
-        stuck_pieces = cv2.max(im1, im2)
+        copy_to(top_fragment_bottom, im1, 0, 0)
+        copy_to(bottom_fragment_top, im2, 0, delta_for_max)
 
-        return max_nonzero, stuck_pieces, im_product_for_max
+        return self.__stick_over(max_adjacency, im2, gap_lines[1], im1, gap_lines[0])
+
+    def __get_bottom_half(self, img):
+        return img[img.shape[0] / 2:, :]
+
+    def __get_top_half(self, img):
+        return img[:img.shape[0] / 2, :]
+
+    def __stick_over(self, adjacency, fragment_above, edge_above, fragment_below, edge_below):
+        above_area = self.edge_detector.getArea(fragment_above)
+        above_area_pixels = np.nonzero(above_area)
+        stuck_fragments = np.copy(fragment_below)
+        stuck_fragments[above_area_pixels] = fragment_above[above_area_pixels]
+        gap_line = cv2.bitwise_or(np.copy(edge_above), cv2.bitwise_and(edge_below, cv2.bitwise_not(above_area)))
+        return FragmentsConnection(adjacency, stuck_fragments, gap_line)
 
 
-class ContentMatcher(object):
+class FragmentsContentMatcher(object):
 
-    def matchContent(self, stuck_pieces, gap_line):
+    def matchFragmentsContent(self, stuck_fragments, gap_line):
         pass
+
+
+class HarrisFragmentsContentMatcher(FragmentsContentMatcher):
+
+    def __init__(self):
+        self.write_result_to = None
+
+    def matchFragmentsContent(self, stuck_fragments, gap_line):
+        gap_line_pixels = np.nonzero(gap_line)
+        fragments_harris = cv2.cornerHarris(stuck_fragments, blockSize=2, ksize=3, k=0.04)
+        harris_values = fragments_harris[gap_line_pixels]
+        if self.write_result_to is not None:
+            img = np.zeros(fragments_harris.shape, fragments_harris.dtype)
+            img[gap_line_pixels] = harris_values
+            img = cv2.normalize(img, img, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
+            cv2.imwrite(self.write_result_to, img)
+            cv2.imwrite('gap_' + self.write_result_to, gap_line)
+        harris_sum = np.sum(np.abs(harris_values))
+        return float(harris_sum) / float(harris_values.shape[0])

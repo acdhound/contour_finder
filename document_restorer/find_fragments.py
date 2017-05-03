2a846d6f0a91416e656387b572d8c1f598c3c197
acdhound <adivanov95@mail.ru>
2017-05-03 21:30:50 +0300

    shadow source image to make contours more distinguishable

--- document_restorer/find_fragments.py
+++ document_restorer/find_fragments.py
@@ -15,11 +15,12 @@ img = cv2.cvtColor(img_colored, cv2.COLOR_BGR2GRAY)
 binarizer = MorphCloseBinarizer(threshold, kernel_size)
 collector = FragmentsCollector(binarizer)
 fragments = collector.collectFragments(img)
+img_colored = cv2.normalize(img_colored, img_colored, 60, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
 for f in fragments:
     cv2.rectangle(img_colored, f.source_rect.topLeft(), f.source_rect.bottomRight(), (0, 0, 255), 1)
     cv2.drawContours(img_colored, [f.source_contour], -1, (0, 255, 0), 1)
     cv2.putText(img_colored, str(fragments.index(f)),
-                (10, f.source_rect.y + f.source_rect.h/2 + 8), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0))
+                (10, f.source_rect.y + f.source_rect.h/2 + 8), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255))
 
 cv2.imwrite('segmented.png', img_colored)
 cv2.imshow('segmented', img_colored)
